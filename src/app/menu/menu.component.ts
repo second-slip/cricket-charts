c72297df7d2cfd306aebcf2f1cabd91e3f428d49
Andrew Cross <andrew-stuart-cross@outlook.com>
2024-07-26 19:26:44 +0100

    better menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss'],
     standalone: true,
-    imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive]
+    imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive, MatIconModule, MatMenuModule]
 })
 export class MenuComponent { }
