69cb74fa4bec8a89f50f053e59a3b7b40c4fc7aa
Andrew Cross <andrew-stuart-cross@outlook.com>
2025-02-01 18:49:00 +0000

    update ideas and change log

--- src/app/ideas/ideas.component.ts
+++ src/app/ideas/ideas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-ideas',
-    imports: [],
-    templateUrl: './ideas.component.html',
-    styleUrl: './ideas.component.scss'
+  selector: 'app-ideas',
+  imports: [RouterLink],
+  templateUrl: './ideas.component.html',
+  styleUrl: './ideas.component.scss',
 })
-export class IdeasComponent { }
+export class IdeasComponent {}
