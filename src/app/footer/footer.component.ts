8dd4caddc776de4a7c70e2b724e6f745b2d1ff91
Andrew Cross <andrew-stuart-cross@outlook.com>
2025-02-01 15:39:03 +0000

    update copyright message

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   protected message = signal('');
 
   constructor() {
     const year = new Date().getFullYear().toString();
-    this.message.set(`\u00A9 ${year} Andrew Stuart Cross`);
+    this.message.set(`\u00A9 2024\u2014${year} Andrew Stuart Cross`);
   }
 }
